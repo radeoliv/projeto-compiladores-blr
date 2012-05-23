97ddd23db3906b21462c39b4322c057592ae0d4b
raos@ecomp.poli.br <raos@ecomp.poli.br@05984061-9f84-a7a2-b579-840aab9fc188>
2012-05-23 02:12:50 +0000

    Código a ser adaptado. Não está correto! Só para ter uma base!

---
+++ trunk/ projeto-compiladores-blr/ProjetoCompiladores/src/encoder/Backup.java
@@ -0,0 +1,446 @@
+package encoder;
+
+import java.util.ArrayList;
+
+import compiler.Properties;
+
+import checker.SemanticException;
+
+import util.Arquivo;
+import util.Visitor;
+import util.AST.AST;
+import util.AST.Program;
+import util.AST.Command.Command;
+import util.AST.Command.FunctionBody;
+import util.AST.Command.FunctionDeclaration;
+import util.AST.Command.VariableDeclaration;
+import util.AST.Expression.BinaryExpression;
+import util.AST.Expression.BooleanUnaryExpression;
+import util.AST.Expression.Expression;
+import util.AST.Expression.IdentifierUnaryExpression;
+import util.AST.Expression.NumberUnaryExpression;
+import util.AST.Expression.UnaryExpression;
+import util.AST.RHS.CallStatementRHS;
+import util.AST.RHS.ExpressionRHS;
+import util.AST.Statement.AssignStatement;
+import util.AST.Statement.BreakStatement;
+import util.AST.Statement.CallStatement;
+import util.AST.Statement.ContinueStatement;
+import util.AST.Statement.IfElseStatement;
+import util.AST.Statement.PrintlnStatement;
+import util.AST.Statement.ReturnStatement;
+import util.AST.Statement.Statement;
+import util.AST.Statement.WhileStatement;
+import util.AST.Terminal.BooleanValue;
+import util.AST.Terminal.Identifier;
+import util.AST.Terminal.NumberValue;
+import util.AST.Terminal.Operator;
+import util.AST.Terminal.Terminal;
+import util.AST.Terminal.Type;
+
+public class Encoder implements Visitor {
+
+	private ArrayList<Instruction> instructions;
+	private int startSectionData = 0;
+	private Arquivo arquivo;
+	private int contConstantesDouble = 1;
+
+	private int contIfElse;
+	
+	public void encode(AST ast) throws SemanticException {
+		instructions = new ArrayList<Instruction>();
+		ast.visit(this, null);
+		gerarArquivo();
+	}
+	
+	private void gerarArquivo(){
+		String entrada = Properties.sourceCodeLocation;
+		String saida = entrada.substring(0, entrada.lastIndexOf('.')+1);
+		saida = saida.concat("asm");
+		arquivo = new Arquivo(entrada,saida);
+		
+		for(Instruction i: instructions){
+			arquivo.println(i.toString());
+			System.out.println(i.toString());
+			//System.out.println(i.getOpCode() +" "+i.getOp1()+" "+i.getOp2()+" "+i.getOp3());
+		}
+		
+		arquivo.close();
+	}
+	
+	public void emit(int opCode, String op1, String op2, String op3){
+		instructions.add(new Instruction(opCode,op1,op2,op3));
+	}
+	
+	public void emit(int opCode, String op1){
+		instructions.add(new Instruction(opCode,op1,null,null));
+	}
+	
+	public void emit(int opCode, String op1,String op2){
+		instructions.add(new Instruction(opCode,op1,op2,null));
+	}
+	
+	public void emit(int opCode, String op1,String op2,int posicao){
+		instructions.add(posicao,new Instruction(opCode,op1,op2,null));
+	}
+
+	public Object visitProgram(Program prog, Object arg)throws SemanticException {
+		
+		emit(InstructionType.EXTERN,InstructionType.PRINTF);
+		emit(InstructionType.SECTION, InstructionType.DATA);
+		startSectionData = instructions.size();
+		
+		for(Command c : prog.getCommands() ){
+			if(c instanceof VariableDeclaration){
+				VariableDeclaration vd = (VariableDeclaration)c;
+				vd.setEscopo(Identifier.GLOBAL);
+				c.visit(this, prog);
+			}
+		}
+		emit(InstructionType.INT_FORMAT,"");
+		emit(InstructionType.DOUBLE_FORMAT,"");
+		
+		emit(InstructionType.SECTION,InstructionType.TEXT);
+		emit(InstructionType.GLOBAL,InstructionType.WINMAIN);
+		
+		for(Command c : prog.getCommands() ){
+			if(c instanceof FunctionDeclaration)c.visit(this, prog);
+		}
+		
+		return prog;
+	}
+	
+	public Object visitVariableDeclaration(VariableDeclaration com, Object arg)	throws SemanticException {
+		com.getIdentifier().visit(this, arg);
+		Identifier id = com.getIdentifier();
+		String s = id.getTipo();
+		if(arg instanceof Program){
+			emit(InstructionType.VARIAVEL_GLOBAL,id.getSpelling(),s);
+			((VariableDeclaration)id.getNodeDeclaration()).setEscopo(Identifier.GLOBAL);
+		}else{
+			((VariableDeclaration)id.getNodeDeclaration()).setEscopo(Identifier.LOCAL);
+		}
+		
+		return null;
+	}
+	
+	public Object visitFunctionDeclaration(FunctionDeclaration com, Object arg)throws SemanticException {
+		
+		com.getFunctionName().visit(this, com);
+		Identifier id = com.getFunctionName();
+		emit(InstructionType.FUNCAO_LABEL,id.getSpelling());
+		
+		int deslocamento=4;
+		for(int i=(com.getParameters().size()-1);i>=0;i--){
+			com.getParameters().get(i).setDeslocamento(deslocamento);
+			if(com.getParameters().get(i).getType().getSpelling().equals("double")){
+				deslocamento+=8;
+			}else
+				deslocamento+=4;	
+		}
+		
+		com.getFunctionBody().visit(this, com);
+		
+		emit(InstructionType.MOV,InstructionType.ESP,InstructionType.EBP);
+		emit(InstructionType.POP,InstructionType.EBP);
+		if(com.getFunctionName().getSpelling().equals("main")) 
+			emit(InstructionType.MOV,InstructionType.EAX,"0");
+		emit(InstructionType.RET,"");
+		
+		return null;
+	}
+	
+	public Object visitFunctionBody(FunctionBody funB, Object arg)throws SemanticException {
+		contIfElse = 1;
+		emit(InstructionType.PUSH, InstructionType.EBP);
+		emit(InstructionType.MOV, InstructionType.EBP, InstructionType.ESP);
+		
+		Integer deslocamento = 0;
+		String tipo;
+		//Conta o deslocamento
+		for(VariableDeclaration vd : funB.getVariables()){
+			tipo = vd.getType().getSpelling();
+			if(tipo.equals("int") || tipo.equals("boolean"))
+				deslocamento += 4;
+			else
+				deslocamento += 8;
+		}
+		//Desloca o ESP
+		if(deslocamento != 0)
+			emit(InstructionType.SUB,InstructionType.ESP,deslocamento.toString());
+		
+		//Atribui o deslocamento
+		for(VariableDeclaration vd : funB.getVariables()){
+			vd.setEscopo(Identifier.LOCAL);
+			tipo = vd.getType().getSpelling();
+			if(tipo.equals("int") || tipo.equals("boolean")){
+				vd.setDeslocamento(deslocamento);
+				deslocamento -= 4;
+			}
+			else{
+				vd.setDeslocamento(deslocamento);
+				deslocamento -= 8;
+			}
+		}
+		for(Statement s : funB.getStatements()){
+			s.visit(this, arg);
+		}
+		return null;
+	}
+	
+	public Object visitAssignStatement(AssignStatement statement, Object arg)throws SemanticException {
+		statement.getVariableName().visit(this, arg);
+		//Empilha
+		statement.getRightHandStatement().visit(this, statement);
+		
+		Identifier id = statement.getVariableName();
+		VariableDeclaration vd = ((VariableDeclaration)id.getNodeDeclaration());
+		
+		Integer deslocamento = vd.getDeslocamento();
+		String tipo = vd.getType().getSpelling();
+		
+		//System.out.println("Indentifier:             "+id.getSpelling());
+		//Desempilha e salva
+		if(tipo.equals("double")) 
+			if(vd.getEscopo() == Identifier.GLOBAL)
+				emit(InstructionType.POP_FLOAT,id.getSpelling());
+			else{
+				emit(InstructionType.POP_FLOAT,InstructionType.EBP,'-'+deslocamento.toString());
+			}
+		else{
+			if(vd.getEscopo() == Identifier.GLOBAL)
+				emit(InstructionType.POP,id.getTipo(),id.getSpelling());
+			else
+				emit(InstructionType.POP,id.getTipo(),InstructionType.EBP,'-'+deslocamento.toString());
+		}
+
+		return null;
+	}
+
+	public Object visitBinaryExpression(BinaryExpression expr, Object arg)throws SemanticException {
+		Expression exp = (Expression) expr;
+		if(exp instanceof UnaryExpression){
+			if(exp instanceof BooleanUnaryExpression){
+				BooleanUnaryExpression ex = (BooleanUnaryExpression) exp;
+			}else if(exp instanceof IdentifierUnaryExpression){
+				IdentifierUnaryExpression ex = (IdentifierUnaryExpression) exp;
+			}else {
+				NumberValue ex = ((NumberUnaryExpression)exp).getNumberValue();
+			}
+			UnaryExpression ex = (UnaryExpression)exp;
+			emit(InstructionType.PUSH,null );
+		}
+		expr.getLeftExpression().visit(this, arg); 
+		expr.getRightExpression().visit(this, arg); 
+		expr.getOperator().visit(this, expr); 
+		return null;
+	}
+
+	public Object visitBooleanUnaryExpression(BooleanUnaryExpression expr,Object arg) throws SemanticException {
+		expr.getBooleanValue().visit(this, arg);
+		return null;
+	}
+
+	public Object visitBooleanValue(BooleanValue value, Object arg)	throws SemanticException {
+		
+		if(value.getSpelling().equals("true")){
+				emit(InstructionType.PUSH,"boolean","true");
+		}else{
+				emit(InstructionType.PUSH,"boolean","false");
+		}
+		return null;
+	}
+
+	public Object visitBreakStatement(BreakStatement statement, Object arg)throws SemanticException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object visitCallStatement(CallStatement statement, Object arg)throws SemanticException {
+		if(arg instanceof AssignStatement){
+			//Empilhando os parametros da função
+			for(Identifier id : statement.getArguments() ){
+				id.visit(this, statement);
+			}
+			emit(InstructionType.CALL_FUNTION, statement.getFunctionName().getSpelling());
+			
+			//limpar valor alocado para os paremetros
+			int deslocamento=0;
+			Identifier id = statement.getFunctionName();
+			FunctionDeclaration fun = ((FunctionDeclaration)id.getNodeDeclaration());
+			for( VariableDeclaration vd : fun.getParameters() ){
+				deslocamento += vd.getDeslocamento();
+			}
+			Integer desloc=new Integer(deslocamento);
+			emit(InstructionType.ADD, InstructionType.ESP, desloc.toString());
+			//Empilhando valor de retorno
+			emit(InstructionType.PUSH, InstructionType.EAX);
+		}
+		return null;
+	}
+
+	public Object visitCallStatementRHS(CallStatementRHS rhs, Object arg)throws SemanticException {
+		rhs.getFunctionCall().visit(this, arg);
+		return null;
+	}
+
+	public Object visitContinueStatement(ContinueStatement statement, Object arg)throws SemanticException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object visitExpressionRHS(ExpressionRHS expRHS, Object arg)throws SemanticException {
+		expRHS.getExpression().visit(this, arg);
+		return null;
+	}
+
+	public Object visitIdentifier(Identifier id, Object arg)throws SemanticException {
+		if(arg instanceof CallStatement){
+			VariableDeclaration vd = ((VariableDeclaration)id.getNodeDeclaration());
+			Integer deslocamento = vd.getDeslocamento();
+			String tipo = vd.getType().getSpelling();
+			
+			if(tipo.equals("double")){
+				if(vd.getEscopo() == Identifier.GLOBAL)
+					emit(InstructionType.PUSH_FLOAT,id.getSpelling());
+				else
+					emit(InstructionType.PUSH_FLOAT,InstructionType.EBP,'-'+deslocamento.toString());
+			}else {
+				if(vd.getEscopo() == Identifier.GLOBAL)
+					emit(InstructionType.PUSH,id.getTipo(),id.getSpelling());
+				else
+					emit(InstructionType.PUSH,id.getTipo(),InstructionType.EBP,'-'+deslocamento.toString());
+			}
+		}
+		return null;
+	}
+
+	public Object visitIdentifierUnaryExpression(IdentifierUnaryExpression expr, Object arg)throws SemanticException {
+		if(arg instanceof AssignStatement){
+			Identifier id = expr.getVariableName();
+			VariableDeclaration vd = ((VariableDeclaration)id.getNodeDeclaration());
+			Integer deslocamento = vd.getDeslocamento();
+			
+			if(expr.getTipo().equals("double")){
+				if(vd.getEscopo() == Identifier.GLOBAL)
+					emit(InstructionType.PUSH_FLOAT,id.getSpelling());
+				else{
+					emit(InstructionType.PUSH_FLOAT,InstructionType.EBP,'-'+deslocamento.toString());
+				}
+				
+			}else {
+				if(vd.getEscopo() == Identifier.GLOBAL)
+					emit(InstructionType.PUSH,id.getTipo(),id.getSpelling());
+				else
+					emit(InstructionType.PUSH,id.getTipo(),InstructionType.EBP,'-'+deslocamento.toString());
+			}
+		}
+		return null;
+	}
+
+	public Object visitIfElseStatement(IfElseStatement statement, Object arg)throws SemanticException {
+		if(arg instanceof FunctionDeclaration){
+			FunctionDeclaration fd = (FunctionDeclaration)arg;
+			for(Statement s: statement.getIfStatements()){
+				s.visit(this, arg);
+			}
+			String labelElse = fd.getFunctionName().getSpelling()+'_'+"else_"+contIfElse+"_block";
+			emit(InstructionType.FUNCAO_LABEL, labelElse);
+			
+			for(Statement s: statement.getElseStatements()){
+				s.visit(this, arg);
+			}
+			String label = fd.getFunctionName().getSpelling()+'_'+"endIf_"+contIfElse;
+			emit(InstructionType.FUNCAO_LABEL, label);
+			contIfElse++;
+		}
+		return null;
+	}
+
+	public Object visitNumberUnaryExpression(NumberUnaryExpression expr,Object arg) throws SemanticException {
+		expr.getNumberValue().visit(this, arg);
+		return null;
+	}
+
+	public Object visitNumberValue(NumberValue value, Object arg)throws SemanticException {
+		if(arg instanceof AssignStatement){
+			if(value.getTipo().equals("double")){
+				String constante = "const"+contConstantesDouble;
+				emit(InstructionType.VARIAVEL_GLOBAL,constante,value.getTipo(),startSectionData);
+				contConstantesDouble++;
+				emit(InstructionType.PUSH_FLOAT,constante);
+			}else{
+				emit(InstructionType.PUSH,value.getTipo(),value.getSpelling());
+			}
+		}
+		return null;
+	}
+
+	public Object visitOperator(Operator operator, Object arg)throws SemanticException {
+		String op=operator.getSpelling();
+		BinaryExpression be = (BinaryExpression)arg;
+		if(be.getTipo().equals("double")){
+			if(op.equals("+")){
+				emit(InstructionType.ADD_FLOAT,InstructionType.ST1);
+			}
+			if(op.equals("-")){
+				emit(InstructionType.SUB_FLOAT,InstructionType.ST1);
+			}
+			if(op.equals("/")){
+				emit(InstructionType.MULT_FLOAT,InstructionType.ST1);
+			}
+			if(op.equals("*")){
+				emit(InstructionType.DIV_FLOAT,InstructionType.ST1);
+			}
+		}else{
+			if(op.equals("+")){
+				
+			}
+			if(op.equals("-")){
+				
+			}
+			if(op.equals("/")){
+				
+			}
+			if(op.equals("*")){
+				
+			}
+		}
+		return null;
+	}
+
+	public Object visitPrintlnStatement(PrintlnStatement statement, Object arg)	throws SemanticException {
+		Identifier id = statement.getVariableName();
+		VariableDeclaration vd = (VariableDeclaration)id.getNodeDeclaration();
+		
+		if(vd.getEscopo() == Identifier.GLOBAL){
+		//	if(!id.getTipo().equals("double"))emit(InstructionType.PUSH,id.getTipo(),"["+id.getSpelling()+"]");
+		}
+		
+		return null;
+	}
+
+	public Object visitReturnStatement(ReturnStatement statement, Object arg)throws SemanticException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object visitType(Type type, Object arg) throws SemanticException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object visitUnaryExpression(UnaryExpression unExp, Object arg)throws SemanticException {
+		unExp.visit(this, arg);
+		return null;
+	}
+
+
+	public Object visitWhileStatement(WhileStatement statement, Object arg)	throws SemanticException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
--- trunk/ projeto-compiladores-blr/ProjetoCompiladores/src/encoder/Instruction.java
+++ trunk/ projeto-compiladores-blr/ProjetoCompiladores/src/encoder/Instruction.java
@@ -1,5 +1,146 @@
 package encoder;
 
 public class Instruction {
+	private int opCode; 
+	private String op1; 
+	private String op2; 
+	private String op3; 
+	
+	public int getOpCode() {
+		return opCode;
+	}
 
+	public void setOpCode(int opCode) {
+		this.opCode = opCode;
+	}
+	
+	public String getOp1() {
+		return op1;
+	}
+
+	public void setOp1(String op1) {
+		this.op1 = op1;
+	}
+
+	public String getOp2() {
+		return op2;
+	}
+
+	public void setOp2(String op2) {
+		this.op2 = op2;
+	}
+
+	public String getOp3() {
+		return op3;
+	}
+
+	public void setOp3(String op3) {
+		this.op3 = op3;
+	}
+
+	
+	public Instruction(int opCode, String op1, String op2, String op3){
+		this.opCode = opCode;
+		this.op1 = op1;
+		this.op2 = op2;
+		this.op3 = op3;
+	}	
+	
+	public String toString(){
+		switch(this.opCode){
+		case InstructionType.EXTERN:
+			return (InstructionType.NAME_EXTERN+" "+this.op1);
+			
+		case InstructionType.SECTION:
+			return (InstructionType.NAME_SECTION+" "+this.op1);
+			
+		case InstructionType.RET:
+			return (InstructionType.NAME_RET);
+			
+		case InstructionType.VARIAVEL_GLOBAL:
+			return (this.op1+": "+ InstructionType.TIPO_INT+ " 0");
+			
+		case InstructionType.INT_FORMAT:
+			return ("intFormat: db "+ "\"%d\""+" 10, 0");
+		
+		case InstructionType.GLOBAL:
+			return ("global "+ this.op1);
+		
+		case InstructionType.FUNCAO_LABEL:
+			if(!this.op1.equals("main"))
+				return ("_"+this.op1+":");
+			else return(InstructionType.WINMAIN+":");
+			
+		case InstructionType.PUSH:
+			return gerarPush();
+			
+		case InstructionType.MOV:
+			return (InstructionType.NAME_MOV+" "+this.op1+", "+this.op2);
+			
+		case InstructionType.POP:
+			return (gerarPop());
+		
+		case InstructionType.ADD:
+			return (InstructionType.WORD_ADD+" "+this.op1+", "+this.op2);
+		
+		case InstructionType.SUB:
+			return (InstructionType.WORD_SUB+" "+this.op1+", "+this.op2);
+			
+		case InstructionType.MULT:
+			return (InstructionType.WORD_MULT+" "+this.op1+", "+this.op2);
+			
+		case InstructionType.DIV:
+			return (InstructionType.WORD_DIV+" "+this.op1+", "+this.op2);
+			
+		case InstructionType.CALL_FUNCTION:
+			return (InstructionType.CALL+" _"+this.op1);
+			
+		default:
+			return (opCode+" "+op1+" "+op2+" "+op3);
+		}
+		
+	}
+	
+	private String gerarPush(){
+		if(this.op1.equals("int")) {
+			if(this.op3 == null || this.op3.equals("")){
+				return (InstructionType.NAME_PUSH+" "+InstructionType.DWORD+" "+this.op2+"");
+			}else{
+				return (InstructionType.NAME_PUSH+" "+InstructionType.DWORD+" "+"["+this.op2+this.op3+"]");
+			}
+		}else
+			if(this.op1.equals("boolean")){
+				if( this.op2.equals("false")){
+					return (InstructionType.NAME_PUSH+" "+InstructionType.DWORD+" 0");
+				}else{
+					if( this.op2.equals("true"))
+						return (InstructionType.NAME_PUSH+" "+InstructionType.DWORD+" 1");
+					else{
+						return (InstructionType.NAME_PUSH+" "+InstructionType.DWORD+" ["+this.op2+"]");
+					}
+				}
+			}
+		return (InstructionType.NAME_PUSH+" "+this.op1);
+	}
+	private String gerarPop(){
+		if(this.op1.equals("int")) {
+			if(this.op3 == null || this.op3.equals("")){
+				return (InstructionType.WORD_POP+" "+InstructionType.DWORD+" ["+this.op2+"]");
+			}else{
+				return (InstructionType.WORD_POP+" "+InstructionType.DWORD+" "+"["+this.op2+this.op3+"]");
+			}
+		}else
+			if(this.op1.equals("boolean")){
+				if( this.op2.equals("false")){
+					return (InstructionType.WORD_POP+" "+InstructionType.DWORD+" 0");
+				}else{
+					if( this.op2.equals("true"))
+						return (InstructionType.WORD_POP+" "+InstructionType.DWORD+" 1");
+					else{
+						return (InstructionType.WORD_POP+" "+InstructionType.DWORD+" ["+this.op2+"]");
+					}
+				}
+			}	
+		return (InstructionType.NAME_PUSH+" "+this.op1);
+	}
 }
